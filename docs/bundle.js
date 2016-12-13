bf4a8eed2625494b06f73d1388ac22c47d67d85e
Cheton Wu <cheton@gmail.com>
2016-12-14 00:31:33 +0800

    v0.5.1

--- docs/bundle.js
+++ docs/bundle.js
@@ -25352,8 +25352,6 @@
 	
 	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 	
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-	
 	var _react = __webpack_require__(7);
 	
 	var _react2 = _interopRequireDefault(_react);
@@ -25364,32 +25362,11 @@
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-	
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-	
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-	
-	var ModalTitle = function (_Component) {
-	    _inherits(ModalTitle, _Component);
-	
-	    function ModalTitle() {
-	        _classCallCheck(this, ModalTitle);
-	
-	        return _possibleConstructorReturn(this, (ModalTitle.__proto__ || Object.getPrototypeOf(ModalTitle)).apply(this, arguments));
-	    }
-	
-	    _createClass(ModalTitle, [{
-	        key: 'render',
-	        value: function render() {
-	            return _react2.default.createElement('div', _extends({}, this.props, {
-	                className: _index2.default.modalTitle
-	            }));
-	        }
-	    }]);
-	
-	    return ModalTitle;
-	}(_react.Component);
+	var ModalTitle = function ModalTitle(props) {
+	    return _react2.default.createElement('div', _extends({}, props, {
+	        className: _index2.default.modalTitle
+	    }));
+	};
 	
 	exports.default = ModalTitle;
 
