fb1d6afe973fe7b51ebc295463726fc4640a3ebe
Cheton Wu <cheton@gmail.com>
2018-01-02 18:33:40 +0800

    Setup tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,22 @@
+import React from 'react';
+import { mount } from 'enzyme';
 import { test } from 'tap';
+import '../setupTests';
+import Modal from '../src';
 
-test('noop', (t) => {
+test('<Modal />', (t) => {
+    const wrapper = mount((
+        <Modal>
+            <Modal.Header>
+                <Modal.Title>
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            </Modal.Body>
+            <Modal.Footer>
+            </Modal.Footer>
+        </Modal>
+    ));
+    t.equal(wrapper.find(Modal).length, 1, 'should render <Modal /> component');
     t.end();
 });
