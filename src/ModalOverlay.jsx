acede9c085cb7e413414c9bb5da90c330ad9c01b
Cheton Wu <cheton@gmail.com>
2018-01-14 11:00:00 +0800

    Unset "overflow: auto" property to prevent unexpected vertical or horizontal scrollbar

--- src/ModalOverlay.jsx
+++ src/ModalOverlay.jsx
@@ -20,12 +20,14 @@ const customStyles = {
         boxShadow: '0 4px 16px rgba(0, 0, 0, .5)',
         border: '1px solid #ccc',
         borderRadius: 0,
+        position: 'absolute',
         top: '50%',
         left: '50%',
         right: 'auto',
         bottom: 'auto',
         marginRight: '-50%',
         padding: 0,
+        overflow: null, // Unset "overflow: auto" property
         msTransform: 'translate(-50%, -50%)', // IE9
         transform: 'translate(-50%, -50%)'
     }
